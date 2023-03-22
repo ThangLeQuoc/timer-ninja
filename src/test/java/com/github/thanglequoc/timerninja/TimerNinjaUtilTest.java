de3acb0f15166270f56522ac132a07a72bb2d490
tle <tle@tripadvisor.com>
2023-03-22 15:34:35 +0700

    Append method modifier to the log output
    - Append the method modifier to the stat output
    - Refactoring, clean up, add test

---
+++ src/test/java/com/github/thanglequoc/timerninja/TimerNinjaUtilTest.java
@@ -0,0 +1,4 @@
+package com.github.thanglequoc.timerninja;
+
+public class TimerNinjaUtilTest {
+}
---
+++ src/test/java/com/github/thanglequoc/timerninja/model/User.java
@@ -0,0 +1,18 @@
+package com.github.thanglequoc.timerninja.model;
+
+import com.github.thanglequoc.timerninja.TimerNinjaTracker;
+
+public class User {
+
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+}
