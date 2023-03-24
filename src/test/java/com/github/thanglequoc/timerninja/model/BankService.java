02acf4532ce9181182a33692b3e95950de5cc2bd
tle <tle@tripadvisor.com>
2023-03-24 16:59:06 +0700

    Update test cases

--- src/test/java/com/github/thanglequoc/timerninja/model/BankService.java
+++ src/test/java/com/github/thanglequoc/timerninja/model/BankService.java
@@ -1,11 +1,28 @@
 package com.github.thanglequoc.timerninja.model;
 
+import com.github.thanglequoc.timerninja.Bank;
 import com.github.thanglequoc.timerninja.TimerNinjaTracker;
 
 /**
      * Dummy service class for testing purpose
      * */
     public class BankService {
+
+        private PaymentService paymentService;
+
+        @TimerNinjaTracker(enabled = false)
+        public BankService() {
+            try {
+                Thread.sleep(90);
+                paymentService = new PaymentService();
+                paymentService.warmUp();
+                paymentService.warmUp2();
+                paymentService.warmUp3();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
     
         @TimerNinjaTracker
         String getBankNumber(User user) {
@@ -30,5 +47,12 @@ import com.github.thanglequoc.timerninja.TimerNinjaTracker;
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
+        }
+
+        @TimerNinjaTracker
+        public void testPaymentServiceConnections() {
+            paymentService.warmUp();
+            paymentService.warmUp2();
+            paymentService.warmUp3();
         }
     }
---
+++ src/test/java/com/github/thanglequoc/timerninja/model/PaymentService.java
@@ -0,0 +1,30 @@
+package com.github.thanglequoc.timerninja.model;
+
+import com.github.thanglequoc.timerninja.TimerNinjaTracker;
+
+public class PaymentService {
+
+    @TimerNinjaTracker
+    public PaymentService() {
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @TimerNinjaTracker
+    public void warmUp() {
+        try {
+            Thread.sleep(150);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @TimerNinjaTracker
+    public void warmUp2() {}
+
+    @TimerNinjaTracker
+    public void warmUp3() {}
+}
