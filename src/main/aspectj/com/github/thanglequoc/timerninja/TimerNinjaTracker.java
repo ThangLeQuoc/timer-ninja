5119ef334ea772565a6b76f784e198d82faf32ac
tle <tle@tripadvisor.com>
2023-03-27 19:30:30 +0700

    Update Javadoc of TimerNinjaTracker annotation

--- src/main/aspectj/com/github/thanglequoc/timerninja/TimerNinjaTracker.java
+++ src/main/aspectj/com/github/thanglequoc/timerninja/TimerNinjaTracker.java
@@ -6,6 +6,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.time.temporal.ChronoUnit;
 
+/**
+ * Annotate any Java method with this annotation to track the total execution time of the method. <br/>
+ * This annotation also supports nested tracking. If the annotated method is invoked from a parent method that is also annotated with
+ * {@code @TimerNinjaTracker}, then the execution time of the method will be added to the existing tracking context.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.CONSTRUCTOR, ElementType.METHOD})
 public @interface TimerNinjaTracker {
@@ -18,7 +23,7 @@ public @interface TimerNinjaTracker {
 
     /**
      * Determine if this tracker should be active
-     * Set to false will disable this tracker statistic from the overall tracking trace result
+     * Set to false will disable this tracker from the overall tracking trace result
      * */
     boolean enabled() default true;
 
