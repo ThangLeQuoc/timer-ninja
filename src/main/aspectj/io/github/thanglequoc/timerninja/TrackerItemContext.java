a3d8d422b8c16bddcb3d09dff8c13e0d597fc0e0
Thang Le Quoc <thanglequoc.it@gmail.com>
2023-04-08 20:24:36 +0700

    Update Javadoc

--- src/main/aspectj/io/github/thanglequoc/timerninja/TrackerItemContext.java
+++ src/main/aspectj/io/github/thanglequoc/timerninja/TrackerItemContext.java
@@ -28,31 +28,60 @@ public class TrackerItemContext {
      * */
     private ChronoUnit timeUnit;
 
+    /**
+     * Constructor of tracker item context
+     * @param pointerDepth The pointer depth of this item
+     * @param methodName The method name of this item
+     * */
     TrackerItemContext(int pointerDepth, String methodName) {
         this.pointerDepth = pointerDepth;
         this.methodName = methodName;
     }
 
+    /**
+     * Get the pointer depth of this item
+     * @return The pointer depth
+     * */
     public int getPointerDepth() {
         return pointerDepth;
     }
 
+    /**
+     * Set the item execution time
+     * @param executionTime The execution time
+     * */
     public void setExecutionTime(long executionTime) {
         this.executionTime = executionTime;
     }
 
+    /**
+     * Get the item execution time
+     * @return item execution time
+     * */
     public long getExecutionTime() {
         return executionTime;
     }
 
+    /**
+     * Set the time unit
+     * @param timeUnit Time unit
+     * */
     public void setTimeUnit(ChronoUnit timeUnit) {
         this.timeUnit = timeUnit;
     }
 
+    /**
+     * Get the time unit of this item
+     * @return The time unit
+     * */
     public ChronoUnit getTimeUnit() {
         return timeUnit;
     }
 
+    /**
+     * Get the method name
+     * @return method name
+     * */
     public String getMethodName() {
         return methodName;
     }
